d03f493dc35c5aa6690c524a9fe2e264ee41de24
Jeremy Bernier <jeremy.s.bernier@gmail.com>
2019-09-17 16:00:39 +0900

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Free and Open Source Timesheet Tracker
+
+Link: [https://timesheettracker.github.io](https://timesheettracker.github.io)
+
+Source: [https://github.com/JeremyBernier/TimesheetTracker](https://github.com/JeremyBernier/TimesheetTracker)
---
+++ precache-manifest.985c280a39e3d0a3884651b7929db70f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8c92fda5906424a8c8b11fe75bfd26",
+    "url": "index.html"
+  },
+  {
+    "revision": "b1e56e3f0b52ce36cc76",
+    "url": "main.js"
+  },
+  {
+    "revision": "9d9af177c48ec566e626c2774f0c56a3",
+    "url": "manifest.json"
+  }
+]);
